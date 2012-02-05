ce477bf45487e00f13bea9da11d7a2a63b4b61c1
Elenka <alekseenko.lena@gmail.com>
2012-02-05 17:49:12 +0300

    project and work-scheme

---
+++ NTI/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  NTI
+//
+//  Created by Елена on 05.02.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ NTI/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  NTI
+//
+//  Created by Елена on 05.02.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
