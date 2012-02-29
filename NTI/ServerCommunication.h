a20e7f7a40dc420474017fa0e95816046b134e9b
Elenka <alekseenko.lena@gmail.com>
2012-02-29 14:54:41 +0300

    #7 server communication

---
+++ NTI/ServerCommunication.h
@@ -0,0 +1,13 @@
+//
+//  ServerCommunication.h
+//  NTI
+//
+//  Created by Елена on 29.02.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ServerCommunication : NSObject
+
+@end
---
+++ NTI/ServerCommunication.m
@@ -0,0 +1,13 @@
+//
+//  ServerCommunication.m
+//  NTI
+//
+//  Created by Елена on 29.02.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ServerCommunication.h"
+
+@implementation ServerCommunication
+
+@end
