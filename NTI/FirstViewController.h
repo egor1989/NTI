2b9cc8b39e33867ea096c0972e3e8597e10c9382
Elenka <alekseenko.lena@gmail.com>
2012-03-07 18:01:46 +0300

    #8 views and logics

---
+++ NTI/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  NTI
+//
+//  Created by Елена on 07.03.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
