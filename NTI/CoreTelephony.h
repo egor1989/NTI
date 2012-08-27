7f76af4d8a28b8297c5173e46a3373ccea86a871
Elenka <alekseenko.lena@gmail.com>
2012-08-27 19:01:30 +0400

    #52

---
+++ NTI/CoreTelephony.h
@@ -0,0 +1,45 @@
+//
+//  CoreTelephony.h
+//  NTI
+//
+//  Created by Елена on 27.08.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+
+
+struct CTServerConnection
+{
+    int a;
+    int b;
+    CFMachPortRef myport;
+    int c;
+    int d;
+    int e;
+    int f;
+    int g;
+    int h;
+    int i;
+};
+
+struct CTResult
+{
+    int flag;
+    int a;
+};
+
+struct CTServerConnection * _CTServerConnectionCreate(CFAllocatorRef, void *, int *); 
+
+int *  _CTServerConnectionCopyMobileIdentity(struct CTResult *,   struct CTServerConnection *,  CFStringRef *);
+
+int *  _CTServerConnectionCopyMobileEquipmentInfo(
+                                                  struct CTResult * Status,
+                                                  struct CTServerConnection * Connection,
+                                                  CFMutableDictionaryRef *Dictionary
+                                                  );
+
+struct CTServerConnection *sc=NULL;
+struct CTResult result;
+
+void callback() { }
+
